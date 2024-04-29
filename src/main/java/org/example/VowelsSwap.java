1185e4ec87f6ce44116f4cbb60728037a800917c
Bhartendu Kumar <bhar32@dhcp-9-239-10-71.c4p-in.ibmmobiledemo.com>
2024-04-29 14:44:57 +0530

    modified code

--- src/main/java/org/example/VowelsSwap.java
+++ src/main/java/org/example/VowelsSwap.java
@@ -31,7 +31,6 @@ public class VowelsSwap {
     }
 
     private static boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
-                || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+        return "aeiouAEIOU".indexOf(c) != -1;
     }
 }
