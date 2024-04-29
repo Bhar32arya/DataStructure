1524c3bce3a1474ff33054e14c5ecf245d3d4135
Bhartendu Kumar <bhar32@dhcp-9-239-10-71.c4p-in.ibmmobiledemo.com>
2024-04-29 13:24:49 +0530

    modified code

---
+++ src/main/java/org/example/ExtractWord.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ExtractWord {
+    public static void main(String[] args) {
+        String str = "a 123 good125   example";
+        String collected = Arrays.stream(str.split("\\s+"))
+                .filter(word -> word.matches("^[a-z]+"))
+                .collect(Collectors.joining(" "));
+        System.out.println(collected);
+    }
+}
